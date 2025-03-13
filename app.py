97869cc2600ccaec33f015c127b7f445f65a9871
lokeshenturi <lokilokesh4031@gmail.com>
2025-03-13 09:35:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask , jsonify
+
+todo = Flask(__name__)
+
+students=[
+    {
+        "id":1,
+        "name":"loki",
+        "age":20
+    },
+    {
+        "id": 2,
+        "name": "lokesh",
+        "age": 25
+    },
+    {
+        "id": 3,
+        "name": "e lokesh",
+        "age": 10
+    }
+]
+@todo.route('/students-list')
+def get_student_list():
+    return jsonify(students)
+
+@todo.route('/students/get/<int:id>')
+def get_students_by_id(id):
+    print(id)
+    for std in students:
+        if std['id'] == id:
+            return jsonify(std)
+        print(std)
+        return "hello"
+
+if __name__ == '__main__':
+    todo.run(
+        host='127.0.0.1',
+        port=5010,
+        debug=True
+    )
